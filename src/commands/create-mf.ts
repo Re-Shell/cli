0d37db78de841eb4965fc3f605820bc47e5cdce8
Umut Korkmaz <umutkorkmaz@outlook.com.tr>
2025-05-12 23:10:10 +0300

    fix(cli): correct prompts import for TypeScript

--- src/commands/create-mf.ts
+++ src/commands/create-mf.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs-extra';
 import * as path from 'path';
 import { exec } from 'child_process';
 import { promisify } from 'util';
-import * as prompts from 'prompts';
+import prompts from 'prompts';
 
 const execAsync = promisify(exec);
 
